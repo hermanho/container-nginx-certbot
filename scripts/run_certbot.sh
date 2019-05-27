7ab3351e961ba958b4d937ca57f765701912cc11
hermanho <hermanho@users.noreply.github.com>
2019-05-28 00:05:49 +0800

    Update run_certbot.sh

--- scripts/run_certbot.sh
+++ scripts/run_certbot.sh
@@ -14,12 +14,14 @@ exit_code=0
 # Ensure nginx bootup first
 curl_ret=0
 sleep 5
-curl --write-out %{http_code} --silent --output /dev/null -L --head --max-time 10 "http://127.0.0.1"
+curl --write-out "%{http_code}\n" --silent --output /dev/null -L --head --max-time 15 "http://127.0.0.1"
 curl_ret=$?
 if [ $curl_ret -ne 0 ]
 then
+    echo "$curl_ret"
     error "Nginx booting error."
     exit_code=1
+    exit $exit_code
 fi
 echo "Nginx ready."
 
