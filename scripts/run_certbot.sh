a970493f07fb696986caf8baaed6e2269ccb711a
hermanho <hermanho@users.noreply.github.com>
2019-05-28 00:21:33 +0800

    Update run_certbot.sh

--- scripts/run_certbot.sh
+++ scripts/run_certbot.sh
@@ -14,7 +14,7 @@ exit_code=0
 # Ensure nginx bootup first
 curl_ret=0
 sleep 5
-curl --write-out "%{http_code}\n" --silent --output /dev/null -L --head --max-time 15 "http://127.0.0.1"
+curl --write-out "%{http_code}\n" --silent --output /dev/null --head --max-time 15 "http://127.0.0.1"
 curl_ret=$?
 if [ $curl_ret -ne 0 ]
 then
