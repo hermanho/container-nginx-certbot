0f73957078bbc49f9c804edb513eab5ca1c91fbf
hermanho <hermanho@users.noreply.github.com>
2019-05-28 00:56:08 +0800

    Allow to config sleep from environment variable

--- scripts/run_certbot.sh
+++ scripts/run_certbot.sh
@@ -11,13 +11,16 @@ fi
 
 exit_code=0
 
+
+if [ -z "$SLEEP_TIMEOUT" ]; then
+    SLEEP_TIMEOUT=15
+fi
 # Ensure nginx bootup first
 curl_ret=0
-sleep 15
+sleep $SLEEP_TIMEOUT
 curl --write-out "%{http_code}\n" --silent --output /dev/null --head --max-time 15 "http://127.0.0.1"
 curl_ret=$?
-if [ $curl_ret -ne 0 ]
-then
+if [ $curl_ret -ne 0 ]; then
     echo "$curl_ret"
     error "Nginx booting error."
     exit_code=1
