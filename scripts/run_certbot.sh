cb76e566783872cb20e51a7fd6c31d18416ea66e
hermanho <hermanho@users.noreply.github.com>
2020-01-06 10:36:38 +0800

    Split code to startup dir

--- scripts/run_certbot.sh
+++ scripts/run_certbot.sh
@@ -10,30 +10,6 @@ if [ -z "$CERTBOT_EMAIL" ]; then
 fi
 
 exit_code=0
-
-
-if [ -z "$SLEEP_TIMEOUT" ]; then
-    SLEEP_TIMEOUT=15
-fi
-echo "Sleep $SLEEP_TIMEOUT seconds."
-# Ensure nginx bootup first
-curl_ret=0
-sleep $SLEEP_TIMEOUT
-curl --write-out "%{http_code}\n" --silent --output /dev/null --head --max-time 15 "http://127.0.0.1"
-curl_ret=$?
-if [ $curl_ret -ne 0 ]; then
-    echo "$curl_ret"
-    error "Nginx booting error."
-    exit_code=1
-    exit $exit_code
-fi
-echo "Nginx ready."
-
-# apt-get remove --purge -y libssl-dev curl && \
-# apt-get autoremove -y 
-# apt-get clean && \
-# rm -rf /var/lib/apt/lists/*
-
 set -x
 # Loop over every domain we can find
 for domain in $(parse_domains); do
