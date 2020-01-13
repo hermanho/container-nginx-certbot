638aa1b4b580bc5f9ee0f8dcdd7549dc781b9103
hermanho <hermanho@users.noreply.github.com>
2020-01-13 14:00:09 +0800

    Update startup_sleep.sh

--- scripts/startup/startup_sleep.sh
+++ scripts/startup/startup_sleep.sh
@@ -11,7 +11,7 @@ echo "Sleep $SLEEP_TIMEOUT seconds."
 # Ensure nginx bootup first
 curl_ret=0
 sleep $SLEEP_TIMEOUT
-curl --write-out "%{http_code}\n" --silent --output /dev/null --head --max-time 15 "http://127.0.0.1"
+curl --write-out "curl http://127.0.0.1 : %{http_code}\n" --silent --output /dev/null --head --max-time 15 "http://127.0.0.1"
 curl_ret=$?
 if [ $curl_ret -ne 0 ]; then
     echo "$curl_ret"
@@ -20,7 +20,7 @@ if [ $curl_ret -ne 0 ]; then
     exit $exit_code
 fi
 echo "Nginx ready."
-
+exit 0
 # apt-get remove --purge -y libssl-dev curl && \
 # apt-get autoremove -y 
 # apt-get clean && \
