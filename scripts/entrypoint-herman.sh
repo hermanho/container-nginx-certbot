7af2fbb0e615d36433a469aa9baeb79d81978f4b
Herman Ho <hermanho@users.noreply.github.com>
2020-04-28 17:46:09 +0800

    Fine tune nginx_conf_content

--- scripts/entrypoint-herman.sh
+++ scripts/entrypoint-herman.sh
@@ -22,12 +22,10 @@ python3 --version
 python3 scripts/create-nginx-config.py
 [ $? -ne 0 ] && exit 1
 
-
 if [ "$ENABLE_NAXSI" == "true" ] || [ "$ENABLE_NAXSI" == "TRUE" ]; then
     echo "ENABLE_NAXSI = $ENABLE_NAXSI"
-    cat <(echo "load_module /etc/nginx/modules/ngx_http_naxsi_module.so; # load naxsi") /etc/nginx/nginx.conf >> /etc/nginx/nginx2.conf
-    mv /etc/nginx/nginx.conf /etc/nginx/nginx.conf.bak
-    mv /etc/nginx/nginx2.conf /etc/nginx/nginx.conf
+    nginx_conf_content=`cat <(echo "load_module /etc/nginx/modules/ngx_http_naxsi_module.so; # load naxsi") /etc/nginx/nginx.conf`
+    echo "$nginx_conf_content" > /etc/nginx/nginx.conf
     mv /etc/nginx/conf.d/naxsi.conf.disable /etc/nginx/conf.d/naxsi.conf
 fi
 
